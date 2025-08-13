bfc82349b60f9bd0da8e3ea49e2df8772bf83779
jsto890 <josephstorey03@gmail.com>
2025-08-14 11:20:44 +1200

    spect raw dcm

--- Scripts/Visualise/interactive_visualise.py
+++ Scripts/Visualise/interactive_visualise.py
@@ -1,7 +1,7 @@
 from nibabel import load
 from nibabel.viewers import OrthoSlicer3D
 
-nii_file = "/Volumes/reseng202500013-ndd-ml/data/raw/MRI/BL/CN/sub-COA00016/anat/sub-COA00016_BL_MRI_CN_T1w.nii.gz"
+nii_file = "/Volumes/reseng202500013-ndd-ml/data/raw/SPECT/PPMI/PD/sub-I248960_PPMI_SPECT_PD/sub-I248960_PPMI_SPECT_PD.nii"
 #nii_file = "/Users/josephstorey/P4P/Templates/PET/FDG_PET.nii.gz"
 
 img = load(nii_file)
