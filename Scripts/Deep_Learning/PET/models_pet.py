1e7e9548d73014c5c737d2683552449131276c8a
jsto890 <josephstorey03@gmail.com>
2025-09-28 11:44:46 +1300

    edit

--- Scripts/Deep_Learning/PET/models_pet.py
+++ Scripts/Deep_Learning/PET/models_pet.py
@@ -175,7 +175,8 @@ def get_3d_model(model_name, num_classes=2, in_channels=1, base_channels=16, use
         
         if use_pretrained:
             print("Loading pretrained ResNet18_3D...")
-            model = resnet18(pretrained=True, spatial_dims=3, n_input_channels=in_channels, num_classes=num_classes)
+            # MONAI MedicalNet pretrained weights require feed_forward=False
+            model = resnet18(pretrained=True, spatial_dims=3, n_input_channels=in_channels, num_classes=num_classes, feed_forward=False)
         else:
             print("Creating ResNet18_3D from scratch...")
             model = resnet18(pretrained=False, spatial_dims=3, n_input_channels=in_channels, num_classes=num_classes)
@@ -190,7 +191,8 @@ def get_3d_model(model_name, num_classes=2, in_channels=1, base_channels=16, use
         
         if use_pretrained:
             print("Loading pretrained ResNet50_3D...")
-            model = resnet50(pretrained=True, spatial_dims=3, n_input_channels=in_channels, num_classes=num_classes)
+            # MONAI MedicalNet pretrained weights require feed_forward=False
+            model = resnet50(pretrained=True, spatial_dims=3, n_input_channels=in_channels, num_classes=num_classes, feed_forward=False)
         else:
             print("Creating ResNet50_3D from scratch...")
             model = resnet50(pretrained=False, spatial_dims=3, n_input_channels=in_channels, num_classes=num_classes)
